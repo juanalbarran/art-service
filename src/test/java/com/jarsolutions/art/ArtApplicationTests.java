e31c7e04f1c19641f1cb809ee81aba9e252cdfa8
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-05-04 15:51:21 +0200

    Springboot
    Springboot boilerplate project

---
+++ src/test/java/com/jarsolutions/art/ArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarsolutions.art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
